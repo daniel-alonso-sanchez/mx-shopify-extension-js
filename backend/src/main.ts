1f74a6fd0530288212252d8e56fd0add04382167
Daniel Alonso <daniel.alonso@magnolia-cms.com>
2024-02-07 19:44:29 +0100

    Added 8080 as default port (customizable via env var)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 instrumentation.init();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
