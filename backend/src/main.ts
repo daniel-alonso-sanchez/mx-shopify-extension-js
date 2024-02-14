f83658e25ed04f554b73f9bd037dea61ef50a228
Daniel Alonso <daniel.alonso@magnolia-cms.com>
2024-02-14 08:45:45 +0100

    added openapi integration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,27 @@
 import instrumentation from './instrumentation/instrumentation';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 instrumentation.init();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
   app.setGlobalPrefix('api');
+
+  function setupOpenapi() {
+    const config = new DocumentBuilder()
+      .setTitle('Shopify Extension API')
+      .setDescription('Shopify Multitenant Extension')
+      .setVersion('1.0')
+      .addTag('products')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
+
+  setupOpenapi();
+
+
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
